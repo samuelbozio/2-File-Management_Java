e7e59759289380ae3af740bccba6100238515a0a
Samuel Bozio <samuelbozio@hotmail.com>
2023-07-12 22:00:25 -0300

    file management 3

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a file path: ");
+        String strPath = sc.nextLine();
+
+        File path = new File(strPath);
+
+        System.out.println(path.getName());
+
+
+        sc.close();
+
+
+
+
+    }
+}
